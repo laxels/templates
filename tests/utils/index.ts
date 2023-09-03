e40342e0dbddf010f70f1c68b5892fbb1a6dca12
Leonardo Axel Setyanto <laxels@gmail.com>
2023-09-02 20:45:05 -0700

    url -> path

--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -3,6 +3,6 @@ import type { Page } from '@playwright/test';
 const INTEGRATION_SERVER_URL = `http://localhost:4173`;
 const INTEGRATION_DEV_SERVER_URL = `http://localhost:4174`;
 
-export async function goto(page: Page, url: string, devServer = false): Promise<void> {
-	await page.goto(`${devServer ? INTEGRATION_DEV_SERVER_URL : INTEGRATION_SERVER_URL}${url}`);
+export async function goto(page: Page, path: string, devServer = false): Promise<void> {
+	await page.goto(`${devServer ? INTEGRATION_DEV_SERVER_URL : INTEGRATION_SERVER_URL}${path}`);
 }
